92c638855258d23ddbf86e7b7a32bc42d76bf48d
ZebraVogel94349 <cowo2312@gmail.com>
2021-05-05 20:21:05 +0200

    Added placing chips

--- src/main.c
+++ src/main.c
@@ -15,28 +15,21 @@
 #include "gfx/palette_gfx.h"
 #include "gfx/gfx.h"
 
-
 gfx_UninitedSprite(rotate_sprite, ro_width, ro_height);//sprite buffer
 
 /*Declaring Variables*/
-uint16_t b, x, s, f, y, z, g, posx, posy, h, i, j, k, l, color, chip;
-int e ,c;
+uint16_t b, x, s, f, y, z, g, posx, posy, h, i, j, k, l, color, chip, chipa, chipb, m, p , o, v, s, t , u, e, c;
 uint8_t a, n;
 kb_key_t key;
+uint16_t bets[50] = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};//bets array
 /*Declaring Variables*/
-
-
-void createTableau()//this functions shows the tableau
-{
-	gfx_RLETSprite_NoClip(tableau,215,15);
-}
 
 void DrawTitle() //This function shows the title
 {
 	gfx_SetTextFGColor(254);//Black text color
 	gfx_SetTextScale(1,1);//Text size for Title
 	gfx_SetTextXY(110,10);//Title position
-	gfx_PrintString("Roulette v0.2.4");//Print title
+	gfx_PrintString("Roulette v0.3.0");//Print title
 }
 
 void DrawCursor() //This function shows the cursor
@@ -72,7 +65,7 @@ void DrawChip(type,cx,cy)
 	if(type == 50){color = 246;}//Purple
 	gfx_SetColor(color);
 	gfx_FillCircle(cx,cy,5);
-	gfx_SetColor(0);
+	gfx_SetColor(245);
 	gfx_FillCircle(cx,cy,3);
 	
 }
@@ -106,13 +99,92 @@ void DrawButtons()//This function creates the Menu
 	gfx_SetTextXY(163,145);
 	gfx_PrintString("50");//Print Text
 	DrawChip(50,187,148);
+}
 
+void correctchip()
+{
+	if(chipa > 0){chipb = 1;} 
+	if(chipa > 1){chipb = 2;} 
+	if(chipa > 4){chipb = 5;} 
+	if(chipa > 9){chipb = 10;} 
+	if(chipa > 19){chipb = 20;} 
+	if(chipa > 49){chipb = 50;} 
+}
+
+void placechip(v, x, y)
+{
+	chipa = 0;
+	chipb = 0;
+	chipa = bets[v];
+	correctchip();
+	if(chipa > 0)
+	{
+		DrawChip(chipb, x, y);
+		if(chipa != chipb)
+		{
+			gfx_SetTextScale(1,1);
+			gfx_SetTextFGColor(0);
+			gfx_SetTextXY(x - 4, y);
+			gfx_PrintInt(bets[v], 1);
+		}
+	}
+}
+
+void createTableau()//this functions shows the tableau
+{
+	gfx_RLETSprite_NoClip(tableau,215,15);
+	for(m=0; m < 39; m++)
+	{
+		chipa = 0;
+		chipb = 0;
+		chipa = bets[m];
+		correctchip();
+		p = m % 3;//column
+		o = m / 3;//row
+		p = p * 18;
+		o = o * 14;
+		if(chipa > 0)
+		{
+			DrawChip(chipb, p + 268, o + 36);
+			if(chipa != chipb)
+			{
+				gfx_SetTextScale(1,1);
+				gfx_SetTextFGColor(0);
+				gfx_SetTextXY(p + 264, o + 36);
+				gfx_PrintInt(bets[m], 1);
+			}
+		}
+	}
+	
+	placechip(39, 286, 22);
+	placechip(40, 250, 36);
+	placechip(41, 250, 50);
+	placechip(42, 250, 64);
+	placechip(43, 250, 78);
+	placechip(44, 250, 120);
+	placechip(45, 250, 162);
+	placechip(46, 250, 176);
+	placechip(47, 250, 190);
+	placechip(48, 250, 204);
+	placechip(49, 250, 218);	
+}
+
+void bet(s, t, u)
+{
+	if(posx == t && posy == u)
+	{
+		if(bets[s] + chip < 51)
+		{
+			bets[s] = bets[s] + chip;
+		}
+	}
 }
 
 void main(void)
 {
 	/*
 	List of important colors:
+	White = 0
 	Black =	254
 	Green = 253
 	Red = 252
@@ -122,6 +194,7 @@ void main(void)
 	Light Blue = 248
 	Dark Blue = 247
 	Purple = 246
+	Light Grey = 245
 	*/
 	posx = 4;
 	posy = 1;
@@ -146,6 +219,10 @@ void main(void)
 	gfx_FillScreen(253);
 	do //Loop everything
 	{
+		for(x=0; x < 50; x++)//Clear bets array
+		{
+			bets[x] = 0;
+		}
 		while (kb_Data[6] != kb_Clear)//Controls
 		{
 			kb_Scan();
@@ -187,12 +264,31 @@ void main(void)
 			if(kb_Data[1] == kb_2nd)//2nd
 			{
 				if(posx == 1 && posy == 4){break;}//spin
+				
 				if(posx == 1 && posy == 5){chip = 1;}
 				if(posx == 1 && posy == 6){chip = 2;}
 				if(posx == 1 && posy == 7){chip = 5;}
 				if(posx == 1 && posy == 8){chip = 10;}
 				if(posx == 1 && posy == 9){chip = 20;}
 				if(posx == 1 && posy == 10){chip = 50;}
+				
+				if((posx == 3 && posy < 15) || (posx == 4 && posy < 15 && posy > 1) || (posx == 5 && posy < 15))//Bet on single number except 0
+				{
+					if(bets[(posy - 2) * 3 + posx - 3] + chip < 51)
+					{
+						bets[(posy - 2) * 3 + posx - 3] = bets[(posy - 2) * 3 + posx - 3] + chip;
+					}
+				}
+				bet(39,4,1);
+				bet(40,2,2);
+				bet(41,2,3);
+				bet(42,2,4);
+				bet(43,2,5);
+				bet(44,2,8);
+				bet(45,2,11);
+				bet(46,2,12);
+				bet(47,2,13);
+				bet(48,2,14);
 			}
 			
 			if((posx == 1 && posy == 1) || (posx == 2 && posy == 1) || (posx == 3 && posy == 1) || (posx == 5 && posy == 1)){posx = 4;}
@@ -201,14 +297,39 @@ void main(void)
 			/*Tableau navigation*/
 			
 			gfx_SwapDraw();
-			for(x = 0; x < 16; x++){gfx_SetColor(253); kb_Scan(); DrawTitle();if(kb_Data[1] == kb_2nd || kb_Data[6] == kb_Clear){break;}} //some delay
+			for(x = 0; x < 45; x++){gfx_SetColor(253); kb_Scan(); DrawTitle();if(kb_Data[6] == kb_Clear){break;}} //some delay
 		}			
 		if (kb_Data[6] == kb_Clear) {break;}//Exit
 		srand(rtc_Time());//Set random seed
 		
+		/*Draw everything*/
+		gfx_SetColor(253);
+		gfx_FillRectangle(160,10,155,220);//Green background
+		DrawTitle();		
+		gfx_SetTextXY(163,167);
+		gfx_PrintInt(chip,2);//Print Text
+		DrawChip(chip,187,170);
+		PrintNumber();
 		DrawCursor();
 		createTableau();
 		DrawButtons();
+		gfx_TransparentSprite_NoClip(rotate_sprite, 17, 69);//Show rotated sprite
+		gfx_RLETSprite_NoClip(pfeil, 59, 36);//Show arrow
+		gfx_SwapDraw();
+		gfx_SetColor(253);
+		gfx_FillRectangle(160,10,155,220);//Green background
+		DrawTitle();		
+		gfx_SetTextXY(163,167);
+		gfx_PrintInt(chip,2);//Print Text
+		DrawChip(chip,187,170);
+		PrintNumber();
+		DrawCursor();
+		createTableau();
+		DrawButtons();
+		gfx_TransparentSprite_NoClip(rotate_sprite, 17, 69);//Show rotated sprite
+		gfx_RLETSprite_NoClip(pfeil, 59, 36);//Show arrow
+		gfx_SwapDraw();
+		/*Draw everything*/
 		
 		/*Generate random amount to rotate*/
 		b = 50000;
